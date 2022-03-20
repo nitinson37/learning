793dabd09dabbcd0e0fdefc83ad36b7e11bc7fd1
Nitin Soni <nitin037@yahoo.com>
2022-03-21 00:17:23 +0100

    iterator pattern exercise

--- designPatterns/Exercise Files/Ch05/05_04/exercise/iterator-pattern-exercise/src/main/java/Main.java
+++ designPatterns/Exercise Files/Ch05/05_04/exercise/iterator-pattern-exercise/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -5,6 +7,14 @@ public class Main {
     Employee zak = new Employee("Zak");
     Employee sarah = new Employee("Sarah");
     Employee anna = new Employee("Anna");
+
+    StaffList staffList = new StaffList(zak, sarah, anna);
+
+    StaffListIterator staffListIterator = staffList.iterator();
+
+    while(staffListIterator.hasNext()){
+      System.out.println(staffListIterator.next().getName());
+    }
   }
 
 }
--- designPatterns/Exercise Files/Ch05/05_04/exercise/iterator-pattern-exercise/src/main/java/StaffList.java
+++ designPatterns/Exercise Files/Ch05/05_04/exercise/iterator-pattern-exercise/src/main/java/StaffList.java
@@ -1,3 +1,19 @@
-public class StaffList {
+import java.util.Iterator;
 
+public class StaffList implements Iterable{
+
+    public Employee[] employees;
+
+    public StaffList(Employee...employees){
+        this.employees = employees;
+    }
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    @Override
+    public StaffListIterator iterator() {
+        return new StaffListIterator(this);
+    }
 }
